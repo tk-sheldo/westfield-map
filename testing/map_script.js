467b0a65c16260ef4ccdaa8fb6ed6e0c183dd470
kiwi <tylerjsheldon@gmail.com>
2021-10-04 10:42:43 -0700

    async fix, font tweaking

--- testing/map_script.js
+++ testing/map_script.js
@@ -1,42 +1,46 @@
 
 // TODO
 // experiment with orthographic globe...?
-// reduce package weight
-// other performance patches
 
 // frees up padding space within squarespace DOM, may need to be updated with time
 d3.select('#viz').select(function() { return this.parentNode.parentNode; })
     .style("padding-left", "0px")
     .style("padding-right", "0px")
 
-//d3.select('#viz').style('height', '90vh')
-
 const viz = d3.select("#viz"),
     dot_size = 3,
     zoom_speed = 2000,
     width = parseInt(viz.style("width")),
-    height = parseInt(viz.style("height"))
+    height = parseInt(viz.style("height")),
+    feat_radius = width / 90,
+    feat_opacity = .1;
 
-function calcScale() {
-
-    if (width / height > 2) {
-        return height / 2.6
+function calcScale(proj) {
+    if (proj == 'patterson') {
+        if (width / height > 2) {
+            return height / 2.6
+        } else {
+            return width / 5
+        }   
     } else {
-        return width / 5
+        if (height / width < 1.7) {
+            return height / 18
+        } else {
+            return width / 11
+        }
     }
-
 }
 
 var projection = {
 
     airocean: d3.geoAirocean()
-                .scale(33)   //make dynamic
+                .scale(calcScale('airocean'))  //make dynamic
                 .angle(210)
                 .translate( [width/2, height*(7/16) ]),
 
     patterson: d3.geoNaturalEarth1()
                 .rotate( [-10, 0] )
-                .scale(calcScale())
+                .scale(calcScale('patterson'))
                 .translate( [width/2, height/2 ]),
 
 }
@@ -45,11 +49,14 @@ const urls = {
 
     shapes: "https://raw.githubusercontent.com/tk-sheldo/westfield-map/c5e1780a78d9ab6683121ea771c1a9540cb00006/100m_countries.json",
 
-    //point_data: "https://raw.githubusercontent.com/tk-sheldo/westfield-map/4889efce42885402c7c2c0008d2ac31f8c764f6c/json_processing/joined.json"
 }
 
 var this_projection = projection.patterson
-if (is_mobile()) { this_projection = projection.airocean }
+
+if (is_mobile()) { 
+    this_projection = projection.airocean 
+    viz.select("#feature-hint").remove() 
+}
 
 const geoPathGenerator = d3.geoPath().projection(this_projection);
 const graticule = d3.geoGraticule();
@@ -57,25 +64,26 @@ const graticule = d3.geoGraticule();
 var zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed)
 var focus = null
 
-const [point_data, features_data] = loadPointData()
+function loadWestfieldData(callback) {
 
-function loadPointData() {
-
-    const pd_spreadsheet_id = '1laJ6v2f77V3VhAX_RprMpZv1gquZ_POQ5-ufZcA2g80'
-    const url = 'https://docs.google.com/spreadsheets/d/' + pd_spreadsheet_id + '/gviz/tq?tqx=out:json';
-    var point_data_txt;
+    const spreadsheet_id = '1laJ6v2f77V3VhAX_RprMpZv1gquZ_POQ5-ufZcA2g80';
+    const url = 'https://docs.google.com/spreadsheets/d/' + spreadsheet_id + '/gviz/tq?tqx=out:json';
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4) {
-            point_data_txt = xmlhttp.responseText;
+            callback(null, xmlhttp.responseText);
         }
     };
-    xmlhttp.open("GET", url, false);
+    xmlhttp.open("GET", url, true);
     xmlhttp.send(null);
 
+}
+
+function parseWestfieldData(txt) {
+
     // pulls json object from the text
-    var pd_json = JSON.parse(point_data_txt.substr(47).slice(0, -2));   //might need to tweak slicing
+    var json = JSON.parse(txt.substr(47).slice(0, -2));   //might need to tweak slicing
 
     // structure of final data object
     var clean_pd = {
@@ -93,12 +101,12 @@ function loadPointData() {
 
     // pulls the column headers from the spreadsheet
     var headers = [];
-    for (var col=0; col<pd_json.table.cols.length; col++) { 
-        headers[col] = pd_json.table.cols[col]["label"]; 
+    for (var col=0; col<json.table.cols.length; col++) { 
+        headers[col] = json.table.cols[col]["label"]; 
     }
 
 
-    for (var row=0; row<pd_json.table.rows.length; row++) {
+    for (var row=0; row<json.table.rows.length; row++) {
 
         // structure of each row object
         var obj = {"type": "Feature",
@@ -111,7 +119,7 @@ function loadPointData() {
 
         for (var header=0; header<headers.length; header++) {
             try {
-                obj.properties[headers[header]] = pd_json.table.rows[row].c[header].v
+                obj.properties[headers[header]] = json.table.rows[row].c[header].v
             }
             catch {
                 obj.properties[headers[header]] = null
@@ -123,20 +131,9 @@ function loadPointData() {
 
         clean_pd.features[row] = obj
 
-        console.log(obj.properties.feature == true);
-
     }
 
-    
-
-    var features_pd = JSON.parse(JSON.stringify(clean_pd));
-    features_pd.features = features_pd.features.filter(point => point.properties.type == "feature");
-
-    console.log(clean_pd);
-    console.log(features_pd);
-
-    return [clean_pd, features_pd]
-
+    return clean_pd;
 }
 
 class ZoomHint {
@@ -239,11 +236,15 @@ var zoom_hint = new ZoomHint()
 // load data for map
 d3.queue()
     .defer(d3.json, urls.shapes)
+    .defer(loadWestfieldData)
     .await(draw_map);
 
-function draw_map(error, shapes) {
+function draw_map(error, shapes, westfield_data) {
 
     if (error) throw error;
+
+
+    // ------------------ BASEMAP -------------------
 
     if (!is_mobile()) {
 
@@ -282,6 +283,12 @@ function draw_map(error, shapes) {
         .attr("height", height)
         .on("click", reset);
 
+
+    // ------------------- WESTFIELD OVERLAY ---------------------
+
+
+    const point_data= parseWestfieldData(westfield_data);
+
     viz.select("#map").append("g")
         .attr("id", "programs")
     .selectAll("circle")
@@ -297,49 +304,41 @@ function draw_map(error, shapes) {
         .attr("r", dot_size)
         .classed('program', true)
         .classed('feature', function(d) {    // FIX
-            console.log(d.properties.feature == true);
-
             return d.properties.feature == true; 
         })
         .attr('class', function(d) {
             return d.properties.type;
         })
-
-
-    const feat_radius = width / 60
         
+    if (is_mobile()) {
+        viz.select("#feature-hint").remove()
+    } else {
+        viz.select("#map").append("g")
+            .attr("id", "features")
+        .selectAll("circle")
+            // filters point data
+            .data(point_data.features.filter(point => point.properties.feature))
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) {
+                return this_projection(d.geometry.coordinates)[0]
+            })
+            .attr("cy", function(d) {
+                return this_projection(d.geometry.coordinates)[1]
+            })
+            .attr("r", feat_radius)
+            .attr("fill-opacity", feat_opacity)
+            .attr('class', function(d) {
+                return d.properties.type;
+            })
+            .classed('feature', true)
+            .on("mouseover", function(d) {
+                d3.select(this).transition()
+                    .duration("800")
+                    .ease(d3.easeQuadOut)
+                    .attr("r", feat_radius*1.7);
+            });
 
-    viz.select("#map").append("g")
-        .attr("id", "features")
-    .selectAll("circle")
-        .data(point_data.features.filter(point => point.properties.feature))
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) {
-            return this_projection(d.geometry.coordinates)[0]
-        })
-        .attr("cy", function(d) {
-            return this_projection(d.geometry.coordinates)[1]
-        })
-        .attr("r", feat_radius)
-        .attr('class', function(d) {
-            return d.properties.type;
-        })
-        .classed('feature', true)
-        .on("mouseover", function(d) {
-            d3.select(this).transition()
-                .duration("800")
-                .ease(d3.easeQuadOut)
-                .attr("r", feat_radius*1.3);
-        })
-        .on("mouseout", function(d) {
-            d3.select(this).transition()
-                .duration("800")
-                .attr("r", feat_radius);
-        })
-
-    // program dots
-    if (!(is_mobile())) {
         add_dot_listeners(viz.selectAll('.feature'))
     }
 }
@@ -395,13 +394,20 @@ function select(program_data, program_ref) {
 function is_mobile() { return (window.innerWidth < 500) }
 
 function bounceFeatures() {
-    d3.selectAll('.feature').transition().duration(1000).attr("r", 10)
-      .on("end", function() {
-        d3.select(this).transition().duration(1000).attr("r", 13)
-          .on("end", function() { bounceFeatures(); });
+    d3.selectAll('.feature').transition()
+        .duration(1300)
+        .ease(d3.easeSinInOut)
+        .attr('fill-opacity', feat_opacity)
+        .attr("r", feat_radius*1.4)
+        .on("end", function() {
+            d3.select(this).transition()
+                .duration(1300)
+                .ease(d3.easeSinInOut)
+                .attr("r", feat_radius)
+                .attr('fill-opacity', .3)
+                .on("end", function() { bounceFeatures(); });
       });
 }
-
 bounceFeatures()
 
 
